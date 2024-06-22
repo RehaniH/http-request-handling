a83716a931d390b6675eafc0ed6d82d4f55de12a
RehaniH <rehani44perera@gmail.com>
2024-06-22 06:48:24 +0530

    handling post requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-request-handling';
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+  onCreatePost(postData: { title: string; content: string }) {
+    console.log(postData)
+
+    // Send Http request
+
+    this.http.post('https://angular-learning-apis-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', postData)
+    .subscribe( response => {
+      console.log(response);
+    });
+    // this.http
+    //   .post(
+    //     'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+    //     postData
+    //   )
+    //   .subscribe(responseData => {
+    //     console.log(responseData);
+    //   });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get('https://angular-learning-apis-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+    .pipe(map((responseData: { [key: string]: any }) => {
+      const postArray = [];
+      for ( const key in responseData){
+        if (responseData.hasOwnProperty(key)){
+          postArray.push({...responseData[key], id: key});
+        }
+      }
+      return postArray;
+    }))
+    .subscribe(posts => {
+      console.log(posts);
+    })
+  }
 }
